e7d4b37e64de99dcbe0a5235ba2e78b31d5ff9dd
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-17 20:24:13 +0530

    Create 09_Recursion.cpp

---
+++ Practice Problems/HACKERRANK 30 Days of Code/09_Recursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 1) return 1;
+    return factorial(n - 1) * n;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    cout << factorial(N);
+
+    return 0;
+}
