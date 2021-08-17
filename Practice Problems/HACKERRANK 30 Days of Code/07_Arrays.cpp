5ee18608d4c17d9633af726b7a78c83108bf64f6
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-17 20:18:13 +0530

    Create 07_Arrays.cpp

---
+++ Practice Problems/HACKERRANK 30 Days of Code/07_Arrays.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N; // array size
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i]; // original array
+    }
+
+    reverse(arr, arr + N);
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " "; // reversed array
+    }
+
+    return 0;
+}
