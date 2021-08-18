05590f73beb9060c5262cfd7328346cd0a59a9af
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-18 20:05:47 +0530

    Create 006_factorial.cpp

---
+++ Practice Problems/HACKEREARTH/Basic Programming/Basics of Input | Output/006_factorial.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+	if(n <= 1){
+		return 1;
+	} else{
+		return n * factorial(n - 1);
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+
+	int fact = factorial(n);
+	cout << fact << endl;
+
+	return 0;
+}
