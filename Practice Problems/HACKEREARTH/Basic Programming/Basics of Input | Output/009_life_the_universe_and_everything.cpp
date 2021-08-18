010d6bb7f8f0d320562b897990044cfaa6469a94
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-18 20:33:42 +0530

    Create 009_life_the_universe_and_everything.cpp

---
+++ Practice Problems/HACKEREARTH/Basic Programming/Basics of Input | Output/009_life_the_universe_and_everything.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int num;
+bool x = true;
+
+while(x){
+
+cin>>num; // input nos.
+
+if (num!= 42) // print nos till num is 42
+cout<<num<<endl; // print nos.
+else
+x= false; // if encountered 42 stop
+}
+
+}
