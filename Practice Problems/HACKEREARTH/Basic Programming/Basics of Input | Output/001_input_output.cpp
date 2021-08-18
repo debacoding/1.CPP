444301d30c5c3880f7dae37650d83de4fe1a2af9
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-18 17:34:53 +0530

    Create 001_input_output.cpp

---
+++ Practice Problems/HACKEREARTH/Basic Programming/Basics of Input | Output/001_input_output.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int i;
+	string s;
+
+	cin >> i;
+	getline(cin >> ws,s);
+
+	cout << (i * 2) << endl;
+	cout << s << endl;
+
+	return 0;
+}
