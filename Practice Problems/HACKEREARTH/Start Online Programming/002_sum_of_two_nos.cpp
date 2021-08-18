9283005710f1d993287540ce5d945d2e90ab0047
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-18 13:27:14 +0530

    Create 002_sum_of_two_nos.cpp

---
+++ Practice Problems/HACKEREARTH/Start Online Programming/002_sum_of_two_nos.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+	int num1, num2;   // Define two integer variables
+	cin >> num1 >> num2;  // Get input for the two variables
+    
+	int sum = 0;
+	sum = num1 + num2;	// Calculate sum
+	cout << sum << endl;   // Print the sum
+
+	return 0;
+}
