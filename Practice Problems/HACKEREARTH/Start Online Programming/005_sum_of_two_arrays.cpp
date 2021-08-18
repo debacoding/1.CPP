1a4b8fad7e8e80b8e8d8146856410b05d115de76
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-18 13:42:02 +0530

    Create 005_sum_of_two_arrays.cpp

---
+++ Practice Problems/HACKEREARTH/Start Online Programming/005_sum_of_two_arrays.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int N, i;
+    cin >> N;
+
+    // Declare the two arrays
+    int numArrayA[N], numArrayB[N], sumArray[N];
+    
+    for(i=0;i<N;i++) sumArray[i]=0;
+    
+    // Get the numArrayA
+    for (i=0; i<N; i++) {
+        cin >> numArrayA[i];
+    }
+    
+    // Get the numArrayB
+    for (i=0; i<N; i++) {
+       cin >> numArrayB[i];
+    }
+
+    // Calculate sum
+    for (i=0; i<N; i++) {
+       sumArray[i] = numArrayA[i] + numArrayB[i];    
+    }
+    
+    // Print the sumArray
+    for (i=0; i<N; i++) {
+        cout << sumArray[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
