56f0b02145138a56f66404920c41977097fb64e0
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-19 18:06:44 +0530

    Create CPP018_Virtual_functions.cpp

---
+++ Practice Problems/HACKERRANK/3. Classes/CPP018_Virtual_functions.cpp
@@ -0,0 +1,108 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// The class Person should have data members name and age
+class Person{
+    public:
+    string name;
+    int age;
+  
+    virtual void getdata() {
+        cin >> this->name >> this->age;
+    }
+    virtual void putdata() {
+        cout << this->name << " " << this->age << endl;
+    }
+};
+
+// The classes Professor and Student should inherit from the class Person.
+// For each object being created of the Professor or the Student class, sequential id's should be assigned to them starting from 1.
+
+/*
+The class Professor should have two integer members: publications and cur_id. There will be two member functions: getdata and putdata. The function getdata should get the input from the user: the name, age and publications of the professor. The function putdata should print the name, age, publications and the cur_id of the professor.
+*/
+class Professor : public Person {
+public:
+    Professor() {
+        this->cur_id = ++id;
+    }
+  
+    int publications;
+    static int id;
+    int cur_id;
+  
+    void getdata() {
+        cin >> this->name >> this->age >> this->publications;
+    }
+    void putdata() {
+        cout << this->name << " "
+            << this->age << " "
+            << this->publications << " "
+            << this->cur_id << endl;
+    }
+};
+int Professor::id = 0;
+
+/*
+The class Student should have two data members: marks, which is an array of size  and cur_id. It has two member functions: getdata and putdata. The function getdata should get the input from the user: the name, age, and the marks of the student in  subjects. The function putdata should print the name, age, sum of the marks and the cur_id of the student.
+*/
+class Student : public Person {
+#define NUM_OF_MARKS 6
+public:
+    Student() {
+        this->cur_id = ++id;
+    }
+  
+    int marks[NUM_OF_MARKS];
+    static int id;
+    int cur_id;
+  
+    void getdata() {
+        cin >> this->name >> this->age;
+        for (int i=0; i<NUM_OF_MARKS; i++) {
+            cin >> marks[i];
+        }
+    }
+    void putdata() {
+        int marksSum = 0;
+        for (int i=0; i<NUM_OF_MARKS; i++) {
+            marksSum += marks[i];
+        }
+        cout << this->name << " "
+            << this->age << " "
+            << marksSum << " "
+            << this->cur_id << endl;
+    }
+};
+int Student::id = 0;
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
