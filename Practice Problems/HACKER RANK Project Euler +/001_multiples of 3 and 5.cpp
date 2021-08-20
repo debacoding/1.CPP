d1ceb84976f7223e87c79ca1c663cdc7eec13b8d
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-20 20:05:21 +0530

    sum of all multiples of 3 or 5 below N

---
+++ Practice Problems/HACKER RANK Project Euler +/001_multiples of 3 and 5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    long int t;
+    cin >> t;
+    while(t--){
+     long int N, multiples_of_5, multiples_of_3, multiples_of_15, sum;
+     cin >> N;
+     
+      multiples_of_3 = (N - 1) / 3;
+      multiples_of_5 = (N - 1) / 5;
+      multiples_of_15 = (N - 1) / 15;
+      sum = 3 * multiples_of_3 * (multiples_of_3 + 1) / 2
+              + 5 * multiples_of_5 * (multiples_of_5 + 1) / 2
+              - 15 * multiples_of_15 * (multiples_of_15 + 1) / 2; 
+      cout << sum << endl;          
+    }
+       
+    return 0;
+}
