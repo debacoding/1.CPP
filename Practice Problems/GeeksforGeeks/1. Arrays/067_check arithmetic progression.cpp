5d941fa6a097e93191433c102a3c9e527c38f49a
Debapriya Paul <89045957+debacoding@users.noreply.github.com>
2021-08-29 21:46:46 +0530

    Basic level
    Given an array of N integers. Write a program to check whether an arithmetic progression can be formed using all the given elements. 
    
    Input:
    N=4
    arr[] = { 0,12,4,8 }
    Output: YES
    Explanation: Rearrange given array as
    {0, 4, 8, 12}  which forms an
    arithmetic progression.

---
+++ Practice Problems/GeeksforGeeks/1. Arrays/067_check arithmetic progression.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    bool checkIsAP(int arr[], int n)
+    {
+        // code here
+        sort(arr,arr + n);
+        
+        int d = arr[1] - arr[0]; // common difference
+        
+        for(int i = 0; i < n - 1; i++){
+            int d1 = arr[i + 1] - arr[i];
+            if(d1 != d){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+         (ob.checkIsAP(arr, n))? (cout << "YES" << endl) :
+                       (cout << "NO" << endl);   
+    }
+ 
+  return 0;
+}
+  // } Driver Code Ends
